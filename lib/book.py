476ccc4a87207be1c2e97a27b4672b09bdcec28e
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-20 21:40:08 -0600

    done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self, title) -> None:
+        self.title = title
+
+    def get_pages(self):
+        return self._page_count
+
+    def set_pages(self, pages):
+        if type(pages) == int:
+            self._page_count = pages
+        else:
+            print("page_count must be an integer")
+            self._page_count = None
+
+    page_count = property(get_pages, set_pages)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+book = Book("How to Scrumb")
+
+book.page_count = "fuck"
+
+print(book.page_count)
